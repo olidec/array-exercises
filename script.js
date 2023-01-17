15748ee423402f83343766def6de5951ff14aaa1
olidec <69003898+olidec@users.noreply.github.com>
2023-01-17 11:27:30 +0100

    solutions

--- script.js
+++ script.js
@@ -3,25 +3,31 @@ document.getElementById('original').innerHTML = beatles
 
 // Exercise 1
 
-
+let beatles = ['Paul', 'Ringo']
+document.getElementById('array-ex1').innerHTML = beatles
 
 
 
 // Exercise 2
 
-
+beatles[3] = 'Harry Potter'
+document.getElementById('array-ex2').innerHTML = beatles
 
 // Exercise 3 
 
-
+beatles[4] = 'Yoko'
+document.getElementById('array-ex3').innerHTML = beatles
 
 // Exercise 4 
 
- beatles
+beatles[10] = 'The new Beatle'
+document.getElementById('array-ex4').innerHTML = beatles
 
 // Exercise 5
 
-
+document.getElementById('array-ex5').innerHTML = `The array has length ${ beatles.length }.`
 
 // Exercise 6
 
+beatles.sort()
+document.getElementById('array-ex6').innerHTML = beatles
