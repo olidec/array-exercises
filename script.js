c06aeb33430a6e3aa601be74c31f85c3eaace124
olidec <69003898+olidec@users.noreply.github.com>
2023-01-17 11:26:51 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let beatles = ['John', 'Paul', 'George', 'Ringo']
+document.getElementById('original').innerHTML = beatles
+
+// Exercise 1
+
+
+
+
+
+// Exercise 2
+
+
+
+// Exercise 3 
+
+
+
+// Exercise 4 
+
+ beatles
+
+// Exercise 5
+
+
+
+// Exercise 6
+
